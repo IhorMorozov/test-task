20af57bb6f01a2dacd6a62adf46e2b6d307849bc
Ihor Morozov <ihor.morozov.dev@gmail.com>
2021-10-11 17:28:54 +0300

    fix bug

--- src/components/CheckboxItem/CheckboxItem.jsx
+++ src/components/CheckboxItem/CheckboxItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './CheckboxItem.module.scss';
+import st from '../ProfileItem/ProfileItem.module.scss';
 
 const CheckboxItem = (props) => {
   const { onToggle, column, isChecked } = props;
@@ -9,10 +10,13 @@ const CheckboxItem = (props) => {
       <input
         type="checkbox"
         className={styles.checkbox}
-        // onChange={onToggle}
+        onChange={onToggle}
         checked={isChecked === 'true'}
       />
-      <p>{column}</p>
+      <span className={st.tooltip}>
+        {column}
+        <span className={st.tooltipText}>Click</span>
+      </span>
     </div>
   );
 };
